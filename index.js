8356ed6b62e8f7903516c9c2f3197d00eeff6ce2
m3oza <pocketmc220@gmail.com>
2026-09-19 21:15:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function getButtons(key) {
 
 // ===== NUKE COMMAND DEFINITION =====
 const nukeCommand = new SlashCommandBuilder()
-    .setName('play')
+    .setName('nuke')
     .setDescription('Ultimate Nuke - Customize and destroy server');
 
 // ===== COMMAND HANDLER =====
